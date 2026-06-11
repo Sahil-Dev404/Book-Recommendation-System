9e71796bcfa27123ee1fe33d4a5eb1372c02f33b
Sahil-Dev404 <2005sahilsaini@gmail.com>
2026-06-12 01:43:20 +0530

    app.py started

---
+++ app.py
@@ -0,0 +1,9 @@
+'''
+using Flask as the backdend
+    GET ->serve index.html
+    POST/recommend ->return book recommendations(JSON)
+    GET ->return title suggestions(JSON)
+    GET/health ->health check for render
+'''
+
+
